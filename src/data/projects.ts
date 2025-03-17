49f945f40160c8f76f467a5f02ae2a4fa46e72f9
“Soumyadeep” <deepsoumya8@gmail.com>
2025-03-18 00:18:33 +0530

    adding updated resume

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,11 +14,11 @@ export const projects: Project[] = [
       "Customizable workflow states",
       "Onboarding Multiple Organization",
     ],
-    challenges: "One of the main challenges was implementing real-time updates on the Kanban Board. So implement the proper logic with use of state management and itteration over the data." 
+    challenges: "One of the main challenges was implementing real-time updates on the Kanban Board. So implemented the proper logic with use of state management and itteration over the data." 
   },
   {
     title: "Apparel Store",
-    description: "An e-commerce platform for a fashion brand that contains basic ecom features with strong implementation behind",
+    description: "An e-commerce platform for a fashion brand that contains basic e-commerce features with strong implementation behind the backend",
     technologies: ["React", "Redux" , "Javascript" , "Vite"],
     image: "https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80",
     demoLink: "https://ecom-project-xi.vercel.app/",
@@ -26,11 +26,11 @@ export const projects: Project[] = [
     features: [
       "Product filtering feature",
       "Search and sorting",
-      "pagination Concept",
+      "Pagination Concept",
       "Cart management",
       "Maintained State with Redux",
       
     ],
-    challenges: "Implementing pagination while products are sorted is something a challange here. So we used Redux for state Management to eliminate this issue"
+    challenges: "Implementing pagination while products are sorted is something a challange here. So we used Redux for state Management to eliminate this issue."
   }
 ];
